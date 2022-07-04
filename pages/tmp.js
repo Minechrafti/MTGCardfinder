8e2585cb55f4ca877ccc9992425c5325e2a337ad
str135208 <str135208@stud.gibb.ch>
2022-07-04 10:50:19 +0200

    deleted tmp.js

--- pages/tmp.js
+++
@@ -1,9 +0,0 @@
-
-
-
-if(typeLine.toLowerCase().includes("creature"));
-if(typeLine.toLowerCase().includes("enchantment"));
-if(typeLine.toLowerCase().includes("artifact"));
-if(typeLine.toLowerCase().includes("instant"));
-if(typeLine.toLowerCase().includes("sorcery"));
-if(typeLine.toLowerCase().includes("enchantment"));
